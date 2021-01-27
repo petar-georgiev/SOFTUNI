735a63d7c84754a99e3533a7cba56eba5ad6bc15
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-01-27 16:15:58 +0200

    no message

---
+++ PROGRAMMING BASICS/src/conditionalStatementAdvancedLab/InvalidNumber.java
@@ -0,0 +1,16 @@
+package conditionalStatementAdvancedLab;
+
+import java.util.Scanner;
+
+public class InvalidNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = Integer.parseInt(scan.nextLine());
+        if(num>=100&&num<=200||num==0){
+            System.out.println();
+        }
+        else {
+            System.out.println("invalid");
+        }
+    }
+}
