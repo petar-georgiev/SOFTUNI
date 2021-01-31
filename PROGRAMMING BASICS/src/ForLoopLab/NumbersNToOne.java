a5fc63edf28b1a2253b909d8da37bf02cd9e244b
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-01-31 10:11:03 +0200

    no message

---
+++ PROGRAMMING BASICS/src/ForLoopLab/NumbersNToOne.java
@@ -0,0 +1,14 @@
+package ForLoopLab;
+
+import java.util.Scanner;
+
+public class NumbersNToOne {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = Integer.parseInt(scan.nextLine());
+        for (int i = num; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
+
