435c7651fea636998d390c71d39a737a3d1b4207
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-02-07 09:20:49 +0200

    no message

---
+++ PROGRAMMING BASICS/src/WhileLoopLab/SumNumbers.java
@@ -0,0 +1,17 @@
+package WhileLoopLab;
+
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        int sum = 0;
+
+        while (sum < n) {
+            int currentNum = Integer.parseInt(scan.nextLine());
+            sum += currentNum;
+        }
+        System.out.println(sum);
+    }
+}
