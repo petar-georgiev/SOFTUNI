c2f48a5d7ff3ed3fd7a88d5ae1870c47dd8a6ee8
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-02-17 18:27:30 +0200

    no message

---
+++ PROGRAMMING BASICS/src/NestedLoopsExercise/SpecialNumbers.java
@@ -0,0 +1,26 @@
+package NestedLoopsExercise;
+
+import java.util.Scanner;
+
+public class SpecialNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                for (int k = 1; k < 10; k++) {
+                    for (int l = 1; l < 10; l++) {
+
+                        if (n % i == 0 && n % j == 0 && n % k == 0 && n % l == 0) {
+                            System.out.printf("%d%d%d%d ",i,j,k,l);
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
