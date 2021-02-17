59dd21fc13d6d7ff2b794cdf989a364dea4e63d3
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-02-18 00:37:48 +0200

    Student_Information.java

---
+++ FUNDAMENTALS MODULE/src/BasicLab/Student_Information.java
@@ -0,0 +1,15 @@
+package BasicLab;
+
+import java.util.Scanner;
+
+public class Student_Information {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String name = scan.nextLine();
+        int age  = Integer.parseInt(scan.nextLine());
+        double grade = Double.parseDouble(scan.nextLine());
+
+        System.out.printf("Name: %s, Age: %d, Grade: %.2f", name, age, grade);
+    }
+}
