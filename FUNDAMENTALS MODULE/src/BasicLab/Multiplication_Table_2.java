9db7b28a6fc0c354373cc19a44780612b85367bb
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-02-22 00:34:03 +0200

    Multiplication_Table_2.java

---
+++ FUNDAMENTALS MODULE/src/BasicLab/Multiplication_Table_2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Multiplication_Table_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+
+    }
+}
