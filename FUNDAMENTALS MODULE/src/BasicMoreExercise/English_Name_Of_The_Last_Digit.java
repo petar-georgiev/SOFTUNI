a721c726c200b41cac62eabd7ce6e489b3aa8a2e
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-03-07 15:03:49 +0200

    English_Name_Of_The_Last_Digit.java

---
+++ FUNDAMENTALS MODULE/src/BasicMoreExercise/English_Name_Of_The_Last_Digit.java
@@ -0,0 +1,45 @@
+package BasicMoreExercise;
+
+import java.util.Scanner;
+
+public class English_Name_Of_The_Last_Digit {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int number = Integer.parseInt(scan.nextLine());
+        int remain = number%10;
+
+        switch (remain){
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+            case 7:
+                System.out.println("seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+        }
+    }
+}
