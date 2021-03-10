a782849b4bdc32001c69abdaa6eb5dc414818434
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-03-10 20:04:07 +0200

    CharsToString.java

---
+++ FUNDAMENTALS MODULE/src/DataTypesAndVariablesLab/CharsToString.java
@@ -0,0 +1,16 @@
+package DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class CharsToString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str1 = scan.nextLine();
+        String str2 = scan.nextLine();
+        String str3 = scan.nextLine();
+
+
+        System.out.printf("%s %s %s",str3,str2,str1);
+
+    }
+}
