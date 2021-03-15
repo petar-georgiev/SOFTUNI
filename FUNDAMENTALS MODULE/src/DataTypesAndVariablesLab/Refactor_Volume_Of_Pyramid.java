c4b1132bf3496eac258da5f6c367d1b1d0b62ce9
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-03-15 21:30:39 +0200

    Refactor_Volume_Of_Pyramid.java

---
+++ FUNDAMENTALS MODULE/src/DataTypesAndVariablesLab/Refactor_Volume_Of_Pyramid.java
@@ -0,0 +1,20 @@
+package DataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class Refactor_Volume_Of_Pyramid {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Length: ");
+        double length = Double.parseDouble(scan.nextLine());
+        System.out.print("Width: ");
+        double width = Double.parseDouble(scan.nextLine());
+        System.out.print("Height: ");
+        double height = Double.parseDouble(scan.nextLine());
+        double volume = (length * width * height) / 3;
+        System.out.printf("Pyramid Volume: %.2f", volume);
+
+
+    }
+}
