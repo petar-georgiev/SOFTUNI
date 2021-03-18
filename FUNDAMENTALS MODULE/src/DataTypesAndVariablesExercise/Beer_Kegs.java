51152acf8ebfee16865a6e76940a6b494a370a74
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-03-18 17:58:19 +0200

    Beer_Kegs.java

---
+++ FUNDAMENTALS MODULE/src/DataTypesAndVariablesExercise/Beer_Kegs.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Beer_Kegs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberInputs = Integer.parseInt(scanner.nextLine());
+
+        double volumeOfTheKeg = 0.0;
+        String biggestKegIs = "";
+        for (int i = 0; i < numberInputs; i++) {
+            String modelOfTheBeerKeg = scanner.nextLine();
+            double radiusOfTheKeg = Double.parseDouble(scanner.nextLine());
+            int heightOnTheKeg = Integer.parseInt(scanner.nextLine());
+
+            double volumeOfInputKegIs = CalculateVolumeOfTheKeg(radiusOfTheKeg, heightOnTheKeg);
+
+            if (volumeOfInputKegIs > volumeOfTheKeg) {
+                volumeOfTheKeg = volumeOfInputKegIs;
+                biggestKegIs = modelOfTheBeerKeg;
+            }
+        }
+
+        System.out.println(biggestKegIs);
+    }
+
+    private static double CalculateVolumeOfTheKeg(double radiusOfTheKeg, int heightOnTheKeg) {
+        return Math.PI * Math.pow(radiusOfTheKeg, 2) * (double) heightOnTheKeg;
+    }
+}
