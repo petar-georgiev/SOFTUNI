6142cc75863c3af385d24909f41a1e935861c84b
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-03-24 18:52:52 +0200

    EvenAndOddSubtraction.java

---
+++ FUNDAMENTALS MODULE/src/ArraysLab/EvenAndOddSubtraction.java
@@ -0,0 +1,27 @@
+package ArraysLab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class EvenAndOddSubtraction {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scan.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                evenSum += number;
+            } else {
+                oddSum += number;
+            }
+
+        }
+            int diff = evenSum-oddSum;
+        System.out.println(diff);
+    }
+}
