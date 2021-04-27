2bbf93055c24f8d1dc5bfd2aaccd4f16c31bc32d
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-04-28 00:22:29 +0300

    SumBigNumbers.java

---
+++ FUNDAMENTALS MODULE/src/ObjectAndClassesLab/SumBigNumbers.java
@@ -0,0 +1,16 @@
+package ObjectAndClassesLab;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class SumBigNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        BigInteger firstNumber = new BigInteger(scan.nextLine());
+        BigInteger secondNumber = new BigInteger(scan.nextLine());
+        BigInteger sum;
+        sum=firstNumber.add(secondNumber);
+        System.out.println(sum);
+    }
+}
