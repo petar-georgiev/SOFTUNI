59089ec32c866d75522b6e9f28dd2bd2a992c4fe
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-05-24 13:57:50 +0300

    StringExplosion.java

---
+++ FUNDAMENTALS MODULE/src/TextProcessingExercise/StringExplosion.java
@@ -0,0 +1,26 @@
+package TextProcessingExercise;
+
+import java.util.Scanner;
+
+public class StringExplosion {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+        StringBuilder sb = new StringBuilder();
+        int power = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (currentChar == '>') {
+                power += Integer.parseInt(String.valueOf(input.charAt(i + 1)));
+                sb.append('>');
+            } else if (power == 0) {
+                sb.append(currentChar);
+            } else {
+                power--;
+            }
+        }
+        System.out.println(sb);
+    }
+}
