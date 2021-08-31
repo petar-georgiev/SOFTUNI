71ad0f0893b2e88f6cbf8816661880b53daa086b
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-08-31 17:34:53 +0300

    Car.java

---
+++ JAVA ADVANCED/DefiningClassesLab/P01_CarInfo/Car.java
@@ -0,0 +1,31 @@
+package DefiningClassesLab.P01_CarInfo;
+
+public class Car {
+    public String brand;
+    public String model;
+    public int horsePower;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("The car is: %s %s - %d HP.",
+                brand,model,horsePower);
+    }
+}
