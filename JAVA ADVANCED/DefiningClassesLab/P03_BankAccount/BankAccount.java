9b8b826bf2d1610e0da7851699d0936620ee6867
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-09-02 20:01:30 +0300

    BankAccount.java

---
+++ JAVA ADVANCED/DefiningClassesLab/P03_BankAccount/BankAccount.java
@@ -0,0 +1,30 @@
+package DefiningClassesLab.P03_BankAccount;
+
+public class BankAccount {
+    private static int accountCount = 1;
+    private static double interestRate = 0.02;
+
+    private int id;
+    private double balance;
+
+    public BankAccount() {
+        this.id = accountCount;
+        accountCount++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+
+    public double getInterest(int years) {
+        return this.balance*interestRate*years;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+}
