1e427872ba21f56ce24b49c78bc6e1866d91ab0c
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-09-11 07:50:10 +0300

    Engine.java

---
+++ JAVA ADVANCED/DefiningClassesExercises/P05_CarSalesman/Engine.java
@@ -0,0 +1,57 @@
+package DefiningClassesExercises.P05_CarSalesman;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Engine {
+    private String model;
+    private int power;
+    private String displacement;
+    private String efficiency;
+
+    public Engine(String model, int power, String displacement, String efficiency) {
+        this.model = model;
+        this.power = power;
+        this.displacement = displacement;
+        this.efficiency = efficiency;
+    }
+
+    public Engine(String model, int power) {
+        this(model,power,"n/a","n/a");
+        this.model = model;
+        this.power = power;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public String getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(String displacement) {
+        this.displacement = displacement;
+    }
+
+    public String getEfficiency() {
+        return efficiency;
+    }
+
+    public void setEfficiency(String efficiency) {
+        this.efficiency = efficiency;
+    }
+}
