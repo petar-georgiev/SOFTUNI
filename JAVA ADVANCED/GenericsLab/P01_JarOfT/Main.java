5573a7adfdfa5d3c6c8c9b817594a2ee03937a65
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-09-15 16:38:52 +0300

    Main.java

---
+++ JAVA ADVANCED/GenericsLab/P01_JarOfT/Main.java
@@ -0,0 +1,10 @@
+package GenericsLab.P01_JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JarOfPickles jar = new JarOfPickles();
+        jar.add(new Pickle());
+
+    }
+}
