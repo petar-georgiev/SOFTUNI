3077c06211dbf6c4b46c5808d540bcaf4227750a
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-09-20 18:42:32 +0300

    Box.java

---
+++ JAVA ADVANCED/GenericsExercises/P02_GenericBoxOfInteger/Box.java
@@ -0,0 +1,14 @@
+package GenericsExercises.P02_GenericBoxOfInteger;
+
+public class Box<T> {
+    private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", this.value.getClass().getName(), value.toString());
+    }
+}
