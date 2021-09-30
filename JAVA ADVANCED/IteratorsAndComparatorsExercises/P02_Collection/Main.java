5fe404ceac68867b45e5c8a548b0c41e72365c9f
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-10-01 01:51:58 +0300

    Main.java

---
+++ JAVA ADVANCED/IteratorsAndComparatorsExercises/P02_Collection/Main.java
@@ -0,0 +1,41 @@
+ package IteratorsAndComparatorsExercises.P02_Collection;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<String> data = Arrays.stream(scan.nextLine().split("\\s+"))
+                .skip(1).collect(Collectors.toList());
+
+        ListyIterator listyIterator = new ListyIterator(data);
+
+
+        String input = scan.nextLine();
+
+        while (!input.equals("END")) {
+            switch (input) {
+                case "HasNext":
+                    System.out.println(listyIterator.hasNext());
+                    break;
+                case "Move":
+                    System.out.println(listyIterator.move());
+                    break;
+                case "Print":
+                    try {
+                        System.out.println(listyIterator.print());
+                    } catch (IllegalStateException ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                    break;
+                case "PrintAll":
+                    listyIterator.printAll();
+                    break;
+            }
+            input = scan.nextLine();
+        }
+    }
+}
