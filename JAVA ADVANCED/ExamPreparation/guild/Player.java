6c39cc4e33ff1b74d2bad70064ec9c0be009cdc8
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-10-14 10:19:07 +0300

    Player.java

---
+++ JAVA ADVANCED/ExamPreparation/guild/Player.java
@@ -0,0 +1,47 @@
+package guild;
+
+public class Player {
+    private String name;
+    private String clazz;
+    private String rank;
+    private String description;
+
+    public Player(String name, String clazz) {
+        this.name = name;
+        this.clazz = clazz;
+        this.rank = "Trial";
+        this.description = "n/a";
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Player %s: %s%n" +
+                "Rank: %s%n" +
+                "Description: %s%n",
+                this.name,this.clazz,this.rank,this.description);
+    }
+}
