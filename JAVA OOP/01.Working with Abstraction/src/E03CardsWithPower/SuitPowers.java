45a6bd6c7486bced7edcd4b001ad5ecd5867ba6f
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-11-11 18:04:32 +0200

    SuitPowers.java

---
+++ JAVA OOP/01.Working with Abstraction/src/E03CardsWithPower/SuitPowers.java
@@ -0,0 +1,18 @@
+package E03CardsWithPower;
+
+public enum SuitPowers {
+    CLUBS(0),
+    DIAMONDS(13),
+    HEARTS(26),
+    SPADES(39);
+
+    private int value;
+
+    SuitPowers(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
