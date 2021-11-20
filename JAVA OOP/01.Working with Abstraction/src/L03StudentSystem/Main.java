89f81dd09477890a8678db874f142f7f4bc1acd8
Petar Georgiev <77115153+petar-georgiev@users.noreply.github.com>
2021-11-20 11:02:18 +0200

    Main.java

---
+++ JAVA OOP/01.Working with Abstraction/src/L03StudentSystem/Main.java
@@ -0,0 +1,11 @@
+package L03StudentSystem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StudentSystem system = new StudentSystem();
+        system.start();
+    }
+}
