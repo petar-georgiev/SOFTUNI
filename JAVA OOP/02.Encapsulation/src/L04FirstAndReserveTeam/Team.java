28778c2c11d9f5290fc3e9343b09a3a4bdbd4c00
Git <georgiev.peter1@gmail.com>
2021-12-16 20:51:38 +0200

    Team.java

---
+++ JAVA OOP/02.Encapsulation/src/L04FirstAndReserveTeam/Team.java
@@ -0,0 +1,35 @@
+package L04FirstAndReserveTeam;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(String name) {
+        this.name = name;
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+    }
+
+    public List<Person> getFirstTeam() {
+      return Collections.unmodifiableList(firstTeam);
+    }
+    public List<Person> getReserveTeam(){
+        return Collections.unmodifiableList(reserveTeam);
+    }
+
+
+    public void addPlayer(Person person) {
+        if (person.getAge() < 40) {
+            firstTeam.add(person);
+        } else if (person.getAge() >= 40){
+            reserveTeam.add(person);
+        }
+    }
+}
