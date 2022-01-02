bad4202684474ab71d9e9bd508363c389a7aa9a1
Git <georgiev.peter1@gmail.com>
2022-01-02 19:26:33 +0200

    BladeKnight.java

---
+++ JAVA OOP/03.Inheritance/src/E03PlayersAndMonsters/BladeKnight.java
@@ -0,0 +1,23 @@
+package E03PlayersAndMonsters;
+
+public class BladeKnight extends DarkKnight{
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public int getLevel() {
+        return super.getLevel();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
