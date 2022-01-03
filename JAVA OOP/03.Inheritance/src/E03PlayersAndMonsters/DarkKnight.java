fd315b209978746f2d2b6a4f686ea6a1086254f1
Git <georgiev.peter1@gmail.com>
2022-01-03 09:40:52 +0200

    DarkKnight.java

---
+++ JAVA OOP/03.Inheritance/src/E03PlayersAndMonsters/DarkKnight.java
@@ -0,0 +1,22 @@
+package E03PlayersAndMonsters;
+
+public class DarkKnight extends Knight{
+    public DarkKnight(String username, int level) {
+        super(username, level);
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public int getLevel() {
+        return super.getLevel();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
