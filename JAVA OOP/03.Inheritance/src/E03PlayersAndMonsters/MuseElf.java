8ea1c2572ea00a6cea8768127770c7f1d01eb130
Git <georgiev.peter1@gmail.com>
2022-01-05 00:20:23 +0200

    MuseElf.java

---
+++ JAVA OOP/03.Inheritance/src/E03PlayersAndMonsters/MuseElf.java
@@ -0,0 +1,22 @@
+package E03PlayersAndMonsters;
+
+public class MuseElf extends Elf{
+    public MuseElf(String username, int level) {
+        super(username, level);
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public int getLevel() {
+        return super.getLevel();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
