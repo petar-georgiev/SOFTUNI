20b870d71cea37c5f4112ca48db60d0632031dbf
Git <georgiev.peter1@gmail.com>
2022-01-05 00:19:55 +0200

    Main.java

---
+++ JAVA OOP/03.Inheritance/src/E03PlayersAndMonsters/Main.java
@@ -0,0 +1,8 @@
+package E03PlayersAndMonsters;
+
+public class Main {
+    public static void main(String[] args) {
+        SoulMaster sm = new SoulMaster("name", 3);
+        System.out.println(sm);
+    }
+}
