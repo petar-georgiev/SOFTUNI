b27d0c09683655550d2416df80bb1cf73dc62813
Git <georgiev.peter1@gmail.com>
2022-01-07 19:37:10 +0200

    SoulMaster.java

---
+++ JAVA OOP/03.Inheritance/src/E03PlayersAndMonsters/SoulMaster.java
@@ -0,0 +1,22 @@
+package E03PlayersAndMonsters;
+
+public class SoulMaster extends DarkWizard{
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public int getLevel() {
+        return super.getLevel();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
