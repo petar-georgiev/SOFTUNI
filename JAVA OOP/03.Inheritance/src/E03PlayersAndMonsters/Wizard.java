cffabeac30cd146b75ce1ace579d87d482338f79
Git <georgiev.peter1@gmail.com>
2022-01-08 18:19:33 +0200

    Wizard.java

---
+++ JAVA OOP/03.Inheritance/src/E03PlayersAndMonsters/Wizard.java
@@ -0,0 +1,22 @@
+package E03PlayersAndMonsters;
+
+public class Wizard extends Hero{
+    public Wizard(String username, int level) {
+        super(username, level);
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public int getLevel() {
+        return super.getLevel();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
