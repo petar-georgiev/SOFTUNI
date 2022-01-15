a93925dbaec6c0b1a3871a28257e2e3c882e06af
Git <georgiev.peter1@gmail.com>
2022-01-16 00:30:37 +0200

    Vehicle.java

---
+++ JAVA OOP/03.Inheritance/src/E04NeedForSpeed/Vehicle.java
@@ -0,0 +1,49 @@
+package E04NeedForSpeed;
+
+public class Vehicle {
+    private static final double DEFAULT_FUEL_CONSUMPTION = 1.5;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public static double getDefaultFuelConsumption() {
+        return DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void drive(double kilometers){
+     double consumedFuel = kilometers * this.fuelConsumption;
+     if(this.fuel >= consumedFuel){
+         double leftFuel = this.fuel - consumedFuel;
+         this.fuel  = leftFuel;
+     }
+    }
+}
