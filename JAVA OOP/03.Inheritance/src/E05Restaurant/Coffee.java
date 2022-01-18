3db05caed9de43300f2dc7ebde6c6fda2e4fdf00
Git <georgiev.peter1@gmail.com>
2022-01-18 21:59:24 +0200

    Coffee.java

---
+++ JAVA OOP/03.Inheritance/src/E05Restaurant/Coffee.java
@@ -0,0 +1,21 @@
+package E05Restaurant;
+
+import java.math.BigDecimal;
+
+public class Coffee extends HotBeverage {
+    private static double COFFEE_MILLILITERS = 50;
+    private static BigDecimal COFFEE_PRICE = new BigDecimal(3.50);
+
+
+    private double caffeine;
+
+    public Coffee(String name, double caffeine) {
+        super(name, COFFEE_PRICE, COFFEE_MILLILITERS);
+        this.caffeine = caffeine;
+    }
+
+    public double getCaffeine() {
+        return caffeine;
+    }
+}
+
