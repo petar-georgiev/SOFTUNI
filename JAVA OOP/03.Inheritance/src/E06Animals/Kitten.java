13d88a6709ebad9cacd9110dc81825ed83413cce
Git <georgiev.peter1@gmail.com>
2022-02-02 23:23:22 +0200

    Kitten.java

---
+++ JAVA OOP/03.Inheritance/src/E06Animals/Kitten.java
@@ -0,0 +1,13 @@
+package E06Animals;
+
+public class Kitten extends Cat{
+
+    public Kitten(String name, int age) {
+        super(name, age, Gender.FEMALE);
+    }
+
+    @Override
+    public String produceSound() {
+        return "Meow";
+    }
+}
