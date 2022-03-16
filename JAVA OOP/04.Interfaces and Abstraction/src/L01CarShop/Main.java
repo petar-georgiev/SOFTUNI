e8e0d86cfad3e4b47b16e3cb9926b2602ea1c520
Git <georgiev.peter1@gmail.com>
2022-03-16 23:23:27 +0200

    Main.java

---
+++ JAVA OOP/04.Interfaces and Abstraction/src/L01CarShop/Main.java
@@ -0,0 +1,8 @@
+package L01CarShop;
+
+public class Main {
+    public static void main(String[] args) {
+    Car seat = new Seat("Leon", "gray", 110, "Spain");
+        System.out.println(seat);
+    }
+}
