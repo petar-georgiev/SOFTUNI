a64e81ca3c9de11d5d5f6be5b3557ec6e3e05c7d
Git <georgiev.peter1@gmail.com>
2022-03-18 19:19:02 +0200

    Car.java

---
+++ JAVA OOP/04.Interfaces and Abstraction/src/L02CarShopExtend/Car.java
@@ -0,0 +1,13 @@
+package L02CarShopExtend;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    Integer TIRES = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+    String countryProduced();
+}
+
