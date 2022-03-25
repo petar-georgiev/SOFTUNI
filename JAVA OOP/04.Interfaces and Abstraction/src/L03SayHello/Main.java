2f53da312a0296e70c22552285b5dc4f8c6aefe1
Git <georgiev.peter1@gmail.com>
2022-03-25 22:27:03 +0200

    Main.java

---
+++ JAVA OOP/04.Interfaces and Abstraction/src/L03SayHello/Main.java
@@ -0,0 +1,18 @@
+package L03SayHello;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Bulgarian("Pesho"));
+        persons.add(new European("Pesho"));
+        persons.add(new Chinese("Pesho"));
+
+        for (Person p : persons) {
+            System.out.println(p.sayHello());
+        }
+    }
+}
