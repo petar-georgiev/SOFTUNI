71c9f472679296685f4b24ec209213617d153e9a
Git <georgiev.peter1@gmail.com>
2022-03-30 21:36:33 +0300

    Chinese.java

---
+++ JAVA OOP/04.Interfaces and Abstraction/src/L04SayHelloExtend/Chinese.java
@@ -0,0 +1,13 @@
+package L04SayHelloExtend;
+
+public class Chinese extends BasePerson {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
