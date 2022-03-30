0a9c0984dd2483ec8c59aa6f74816ed4b09778f8
Git <georgiev.peter1@gmail.com>
2022-03-30 21:36:55 +0300

    European.java

---
+++ JAVA OOP/04.Interfaces and Abstraction/src/L04SayHelloExtend/European.java
@@ -0,0 +1,14 @@
+package L04SayHelloExtend;
+
+public class European extends BasePerson {
+
+
+    public European(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello(){
+        return "Hello";
+    }
+}
