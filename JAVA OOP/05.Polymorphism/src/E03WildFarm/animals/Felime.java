7ff75d266ec4cac7ca704ab85dbcb03a708079ce
Git <georgiev.peter1@gmail.com>
2022-04-15 23:56:16 +0300

    Felime.java

---
+++ JAVA OOP/05.Polymorphism/src/E03WildFarm/animals/Felime.java
@@ -0,0 +1,7 @@
+package E03WildFarm.animals;
+
+public abstract class Felime extends Mammal{
+    public Felime(String animalName, String animalType, Double animalWeigh, String livingRegion) {
+        super(animalName, animalType, animalWeigh, livingRegion);
+    }
+}
