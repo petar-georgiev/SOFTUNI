9cc36f63757ce14c58ad9bcb6f0ff9fdbdb18fe4
Git <georgiev.peter1@gmail.com>
2022-04-16 15:10:15 +0300

    Mammal.java

---
+++ JAVA OOP/05.Polymorphism/src/E03WildFarm/animals/Mammal.java
@@ -0,0 +1,10 @@
+package E03WildFarm.animals;
+
+public abstract class Mammal extends Animal{
+
+
+    public Mammal(String animalName, String animalType, Double animalWeigh, String livingRegion) {
+        super(animalName, animalType, animalWeigh,livingRegion);
+
+    }
+}
