ff23847bc99107c8bb5637da63da06ff379c99f8
Git <georgiev.peter1@gmail.com>
2022-04-17 08:31:21 +0300

    Food.java

---
+++ JAVA OOP/05.Polymorphism/src/E03WildFarm/foods/Food.java
@@ -0,0 +1,13 @@
+package E03WildFarm.foods;
+
+public abstract class Food {
+    private int quantity;
+
+    protected Food(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
