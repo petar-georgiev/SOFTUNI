fb9e3873d5cceed8232b2adc790ce55e16794a10
Git <georgiev.peter1@gmail.com>
2022-06-12 23:37:04 +0300

    SaltwaterFish.java

---
+++ JAVA OOP/Exam - 10 April 2021/src/aquarium/entities/fish/SaltwaterFish.java
@@ -0,0 +1,13 @@
+package aquarium.entities.fish;
+
+public class SaltwaterFish extends BaseFish {
+    public SaltwaterFish(String name, String species, double price) {
+        super(name, species, price);
+        setSize(5);
+    }
+
+    @Override
+    public void eat() {
+        setSize(getSize() + 2);
+    }
+}
