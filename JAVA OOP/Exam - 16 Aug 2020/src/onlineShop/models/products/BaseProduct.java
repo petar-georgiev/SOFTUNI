a8618b4fc7b2925481e3189555963409e6b74d82
petar-georgiev <georgiev.peter1@gmail.com>
2022-08-28 20:59:59 +0300

    BaseProduct.java

---
+++ JAVA OOP/Exam - 16 Aug 2020/src/onlineShop/models/products/BaseProduct.java
@@ -0,0 +1,50 @@
+package onlineShop.models.products;
+
+public abstract class BaseProduct implements Product{
+    private int id;
+    private String manufacturer;
+    private String model;
+    private double price;
+    private double overallPerformance;
+
+    public BaseProduct(int id, String manufacturer, String model, double price, double overallPerformance) {
+        this.id = id;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.price = price;
+        this.overallPerformance = overallPerformance;
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getManufacturer() {
+        return this.manufacturer;
+    }
+
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public double getOverallPerformance() {
+        return this.overallPerformance;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Overall Performance: %.2f. " +
+                "Price: %.2f - %s: " +
+                "%s %s (Id: %d)",
+                overallPerformance,price,getClass().getSimpleName(),manufacturer,model,price) ;
+    }
+}
