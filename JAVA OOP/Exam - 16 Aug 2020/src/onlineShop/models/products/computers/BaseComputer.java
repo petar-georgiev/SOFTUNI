5cbd6e34813d460f62cbacb093acdbe0b5713474
petar-georgiev <georgiev.peter1@gmail.com>
2022-09-01 23:55:50 +0300

    BaseComputer.java

---
+++ JAVA OOP/Exam - 16 Aug 2020/src/onlineShop/models/products/computers/BaseComputer.java
@@ -0,0 +1,74 @@
+package onlineShop.models.products.computers;
+
+import onlineShop.models.products.BaseProduct;
+import onlineShop.models.products.components.Component;
+import onlineShop.models.products.peripherals.Peripheral;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseComputer extends BaseProduct implements Computer {
+    private List<Component> components;
+    private List<Peripheral> peripherals;
+
+    public BaseComputer(int id, String manufacturer, String model, double price, double overallPerformance) {
+        super(id, manufacturer, model, price, overallPerformance);
+        this.components = new ArrayList<>();
+        this.peripherals = new ArrayList<>();
+    }
+
+    @Override
+    public int getId() {
+        return super.getId();
+    }
+
+    @Override
+    public String getManufacturer() {
+        return super.getManufacturer();
+    }
+
+    @Override
+    public String getModel() {
+        return super.getModel();
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice();
+    }
+
+    @Override
+    public double getOverallPerformance() {
+        return super.getOverallPerformance();
+    }
+
+    @Override
+    public List<Component> getComponents() {
+        return this.components;
+    }
+
+    @Override
+    public List<Peripheral> getPeripherals() {
+        return this.peripherals;
+    }
+
+    @Override
+    public void addComponent(Component component) {
+
+    }
+
+    @Override
+    public Component removeComponent(String componentType) {
+        return null;
+    }
+
+    @Override
+    public void addPeripheral(Peripheral peripheral) {
+
+    }
+
+    @Override
+    public Peripheral removePeripheral(String peripheralType) {
+        return null;
+    }
+}
