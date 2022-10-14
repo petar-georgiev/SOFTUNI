6541187881e85913d10b0e9abea8f1f44b08e4e1
petar-georgiev <georgiev.peter1@gmail.com>
2022-10-14 22:43:04 +0300

    Gun.java

---
+++ JAVA OOP/Exam - 19 Dec 2020/src/viceCity/models/guns/Gun.java
@@ -0,0 +1,14 @@
+package viceCity.models.guns;
+
+public interface Gun {
+    String getName();
+
+    int getBulletsPerBarrel();
+
+    boolean canFire();
+
+    int getTotalBullets();
+
+    int fire();
+
+}
