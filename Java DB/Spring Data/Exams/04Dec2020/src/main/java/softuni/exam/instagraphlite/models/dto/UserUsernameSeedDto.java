fb46d531af56a4a1699e836f821ae3d71b1a251f
petar-georgiev <georgiev.peter1@gmail.com>
2023-01-12 19:10:14 +0200

    UserUsernameSeedDto.java

---
+++ Java DB/Spring Data/Exams/04Dec2020/src/main/java/softuni/exam/instagraphlite/models/dto/UserUsernameSeedDto.java
@@ -0,0 +1,21 @@
+package softuni.exam.instagraphlite.models.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class UserUsernameSeedDto {
+    private String username;
+
+    @NotBlank
+    @Size(min = 2, max = 18)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
