2cfbd3b6c7c9e1000c396a904f617be1a7ede221
petar-georgiev <georgiev.peter1@gmail.com>
2023-01-12 19:09:47 +0200

    UserSeedDto.java

---
+++ Java DB/Spring Data/Exams/04Dec2020/src/main/java/softuni/exam/instagraphlite/models/dto/UserSeedDto.java
@@ -0,0 +1,56 @@
+package softuni.exam.instagraphlite.models.dto;
+
+import com.google.gson.annotations.Expose;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+public class UserSeedDto {
+
+    @Expose
+    private String username;
+
+    @Expose
+    private String password;
+
+    @Expose
+    private String profilePicture;
+
+
+    public UserSeedDto() {
+    }
+
+    @Size(min = 2, max = 18)
+    @NotBlank
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Size(min = 4)
+    @NotBlank
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @NotBlank
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+
+}
