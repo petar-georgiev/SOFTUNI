aed19fb71b3ab3f2a79a1e20324ab5058031f3ae
petar-georgiev <georgiev.peter1@gmail.com>
2023-01-26 19:02:30 +0200

    ValidationUtil.java

---
+++ Java DB/Spring Data/Exams/04Dec2020/src/main/java/softuni/exam/instagraphlite/util/ValidationUtil.java
@@ -0,0 +1,9 @@
+package softuni.exam.instagraphlite.util;
+
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+public interface ValidationUtil {
+    <E> boolean isValid(E entity);
+    // <E> Set<ConstraintViolation<E>> violation(E entity);
+}
