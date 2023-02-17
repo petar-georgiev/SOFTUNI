c3672e7490688d0cd49790c66032154e1758e654
petar-georgiev <georgiev.peter1@gmail.com>
2023-02-17 07:15:23 +0200

    SellerSeedDto.java

---
+++ Java DB/Spring Data/Exams/29Mar2020/src/main/java/softuni/exam/models/dto/SellerSeedDto.java
@@ -0,0 +1,76 @@
+package softuni.exam.models.dto;
+
+import jdk.jfr.Name;
+import softuni.exam.models.entity.enums.Rating;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@Name("seller")
+public class SellerSeedDto {
+    @XmlElement(name = "first-name")
+    private String firstName;
+    @XmlElement(name = "last-name")
+    private String lastName;
+    @XmlElement
+    private String email;
+    @XmlElement
+    private Rating rating;
+    @XmlElement
+    private String town;
+
+    public SellerSeedDto() {
+    }
+
+    @Size(min = 2, max = 19)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Size(min = 2, max = 19)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @NotNull
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    @NotBlank
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
