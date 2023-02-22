a0b0ab0a51d334c8ebe971ece3c586a4a710ee34
petar-georgiev <georgiev.peter1@gmail.com>
2023-02-22 23:20:46 +0200

    Offer.java

---
+++ Java DB/Spring Data/Exams/29Mar2020/src/main/java/softuni/exam/models/entity/Offer.java
@@ -0,0 +1,86 @@
+package softuni.exam.models.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity(name = "offers")
+public class Offer extends BaseEntity{
+    private BigDecimal price;
+    private String description;
+    private Boolean hasGoldStatus;
+    private LocalDateTime addedOn;
+    private Car car;
+    private Seller seller;
+    private Set<Picture> pictures;
+
+    public Offer() {
+    }
+
+    @ManyToMany
+    public Set<Picture> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(Set<Picture> pictures) {
+        this.pictures = pictures;
+    }
+
+
+    @ManyToOne
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    @ManyToOne
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Column(columnDefinition = "TEXT")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getHasGoldStatus() {
+        return hasGoldStatus;
+    }
+
+    public void setHasGoldStatus(Boolean hasGoldStatus) {
+        this.hasGoldStatus = hasGoldStatus;
+    }
+
+    public LocalDateTime getAddedOn() {
+        return addedOn;
+    }
+
+    public void setAddedOn(LocalDateTime addedOn) {
+        this.addedOn = addedOn;
+    }
+}
+
